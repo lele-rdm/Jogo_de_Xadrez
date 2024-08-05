af6d593741f89ff6626f6342dfc1151c4919f410
Leandro Martins <leandromartinspinto2004@gmail.com>
2024-08-05 16:29:55 -0300

    Fazendo o tratamento de algumas exceções personalizadas

--- Jogo_de_Xadrez/Program.cs
+++ Jogo_de_Xadrez/Program.cs
@@ -8,13 +8,21 @@ namespace Jogo_de_Xadrez
     {
         static void Main(string[] args)
         {
-            Tabuleiroo tab = new Tabuleiroo(8, 8);
+            try 
+            {
+                Tabuleiroo tab = new Tabuleiroo(8, 8);
 
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-            tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-            tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(2, 4));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
+                tab.ColocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 9));
+                tab.ColocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
 
-            Tela.ImprimirTabuleiro(tab);
+                Tela.ImprimirTabuleiro(tab);
+            }
+            catch (TabuleirooException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
 
         }
     }
---
+++ Jogo_de_Xadrez/Tabuleiro/TabuleirooException.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Jogo_de_Xadrez.Tabuleiro
+{
+    class TabuleirooException : Exception
+    {
+        public TabuleirooException(string msg) : base(msg)
+        {
+        }
+
+
+
+    }
+}
